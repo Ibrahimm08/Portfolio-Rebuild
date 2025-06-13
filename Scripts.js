bac2259cd1101b0ae42f879b5d02dd07eb542d3b
Mohammed Maqsood <mohammedmaqsood@Mohammeds-MBP.localdomain>
2025-06-13 15:06:05 +0100

    Added project content

--- Scripts.js
+++ Scripts.js
@@ -1,8 +1,11 @@
 function CatagorySelect(){
 
     const Catagory = document.querySelectorAll(".Catagory");
-    const pill = document.querySelector(".MoveablePill")
-;    
+    const pill = document.querySelector(".MoveablePill");    
+    const Projects = document.getElementById("ProjectList");
+    const ProjectItem = Projects.getElementsByTagName("div");
+    var a
+
     Catagory.forEach((catagory) => {
         pill.classList.remove(catagory.id);
         catagory.addEventListener("click", (event) => {
@@ -11,6 +14,28 @@ function CatagorySelect(){
                 return;
             }
             pill.classList.add(catagory.id);
+
+            // var count = 0;
+            // for(i = 0; i < ProjectItem.length; i++){
+            //     if(ProjectItem[i].id.includes(catagory.id)){
+            //         ProjectItem[i].style.display = "flex";
+                    
+            //         for(var x = 0; x < ProjectItem[i].children.length; x++){
+            //             ProjectItem[i].children[x].style.display = "block";
+            //             console.log(ProjectItem[i].children[x])
+            //         }
+                    
+            //         count++;
+            //     } else{
+            //         ProjectItem[i].style.display = "none";
+            //     }
+            // }
+
+            // if(count == 0){
+            //     document.getElementById("NoResult").style.display="block";
+            // }else{
+            //     document.getElementById("NoResult").style.display="none";
+            // }
         })
     })
 }
